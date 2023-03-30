bd006a2ad6d0e0292decf193e0ab01c7e0a2b4a2
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-03-30 11:56:34 +0200

    Add files via upload
    kalkulator part 2

--- petle_kalkulator.py
+++ petle_kalkulator.py
@@ -2,6 +2,8 @@ while True:
     print("""
     1. Dodawanie
     2. Odejmowanie
+    3. Mnożenie
+    4. DZielenie
     5. Koniec
     """)
     odp = input("Podaj dzialanie")
@@ -10,7 +12,16 @@ while True:
     a = int(input("Podaj pierwsza liczbe"))
     b = int(input("Podaj druga liczbe"))
     if odp == "1":
-        print(a + b)
+        print(f"Wynik działania {a} + {b} = {a + b}")
         # wynik działania 5 + 4 = 9
+    elif odp == "2":
+        print(f"Wynik działania {a} - {b} = {a - b}")
+    elif odp == "3":
+        print(f"Wynik działania {a} * {b} = {a * b}")
+    elif odp == "4":
+        if b != 0:
+            print(f"Wynik działania {a} / {b} = {a / b}")
+        else:
+            print("nie dzielimy przez zero")
     else:
-        print("Nie znam takiego działąnia")
+        print("Nie znam takiego działania")
